8cf77a8b61f1a700b0939f4a6e6dfafe8182fff0
Juan Jose Gallego <jjosegallegomesa@gmail.com>
2023-09-03 17:25:19 -0500

    se crea ejemplo de clase

---
+++ src/main/java/com/example/TurismoaApplication/modelos/Oferta.java
@@ -0,0 +1,13 @@
+package com.example.TurismoaApplication.modelos;
+
+import java.time.LocalDate;
+
+public class Oferta {
+    private Integer id;
+    private String titulo;
+    private String descripcion;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private Double CostoPersona;
+
+}
